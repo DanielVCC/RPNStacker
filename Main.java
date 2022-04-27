2cb7727f3a4def7669afc6b0013f8bfb24a4f4d9
Daniel Cavalcante <dvictor.cintra@gmail.com>
2022-04-27 02:48:05 -0300

    Update Task 02

--- Main.java
+++ Main.java
@@ -1,8 +1,11 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Stack;
+
+import entities_enum.OrderStatus;
 
 public class Main {
 
@@ -27,22 +30,60 @@ public class Main {
 		System.out.println("Enter file path: ");
 		String path = scan.nextLine();
 		Stack<Double> stack = new Stack<Double>();
+		ArrayList<Token> tokens = new ArrayList<Token>();
 
 		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 			String line = br.readLine();
+
 			while (line != null) {
-				// ou para considerar valores negativos
-				if (Character.isDigit(line.charAt(0)) || line.length() > 1 && line.charAt(0) == '-')
-					stack.add(Double.parseDouble(line));
-				else
-					stack.add(operation(stack.pop(), stack.pop(), line));
+				//verifica se a linha equivale a algum operador
+				if (line.equals("+"))
+					tokens.add(new Token(TokenType.valueOf("PLUS"), line));
+				else if (line.equals("-"))
+					tokens.add(new Token(TokenType.valueOf("MINUS"), line));
+				else if (line.equals("*"))
+					tokens.add(new Token(TokenType.valueOf("STAR"), line));
+				else if (line.equals("/"))
+					tokens.add(new Token(TokenType.valueOf("SLASH"), line));
+				
+				/* Ja que a linha nao e aquivalente a um caractere unico equivalente a
+				 * um operador, verificaremos cada caracter da linha para confirmar que
+				 * se trata ou nao de uma sequencia equivalente a um numero */
+				
+				else {
+					String value = "";
+					for(int i = 0; i < line.length(); i++) {
+						if(Character.isDigit(line.charAt(i)) || i == 0 && line.charAt(i) == '-')
+							value = value + line.charAt(i);
+						else
+							throw new UnexpectedCharacterException("Unexpected character: " + line.charAt(i));
+					}
+					tokens.add(new Token(TokenType.valueOf("NUM"), value));
+				} 
 				line = br.readLine();
 			}
-			System.out.println(stack.peek());
+
+			// lista de tokens sera interpretada para as operacoes com a pilha
+			for (int i = 0; i < tokens.size(); i++) {
+				if (tokens.get(i).type.equals(TokenType.valueOf("NUM"))) {
+					stack.add(Double.parseDouble(tokens.get(i).lexeme));
+					//print token
+					System.out.println(tokens.get(i).toString());
+				} else {
+					stack.add(operation(stack.pop(), stack.pop(), tokens.get(i).lexeme));
+					//print token
+					System.out.println(tokens.get(i).toString());
+				}
+			}
+
+			System.out.println("\nSaida: " + stack.peek());
 
 		} catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
+		} catch (UnexpectedCharacterException e) {
+			System.out.println("Error: " + e.getMessage());
 		}
+		
 		scan.close();
 
 	}
---
+++ UnexpectedCharacterException.java
@@ -0,0 +1,13 @@
+
+public class UnexpectedCharacterException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public UnexpectedCharacterException(String msg) {
+		super(msg);
+	}
+
+}
